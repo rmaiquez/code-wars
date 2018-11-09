6e4ac2afb1f1f0181f3c412b6a13989dd7f2f4a7
Ruben Maiquez <r.maikez@gmail.com>
2018-11-09 19:38:47 +0100

    Add katas solutions 8kyu

---
+++ python/8kyu/get-planet-name-by-id.py
@@ -0,0 +1,30 @@
+# The function is not returning the correct values. Can you figure out why?
+# #
+# # Code:
+# # def get_planet_name(id):
+# #     # This doesn't work; Fix it!
+# #     name=""
+# #     switch id:
+# #         case 1: name = "Mercury"
+# #         case 2: name = "Venus"
+# #         case 3: name = "Earth"
+# #         case 4: name = "Mars"
+# #         case 5: name = "Jupiter"
+# #         case 6: name = "Saturn"
+# #         case 7: name = "Uranus"
+# #         case 8: name = "Neptune"
+# #     return name
+
+def get_planet_name(id):
+    name=""
+    switcher = {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus",
+        8: "Neptune",
+        }
+    return switcher.get(id)
